4ab54a568c8431b5043d7c548e33fd200bec9afd
Gerry Shaw <gerry_shaw@yahoo.com>
2020-12-23 21:08:42 -0800

    Updates from Dec 2020

--- fish/functions/custom_shell_init.fish
+++ fish/functions/custom_shell_init.fish
@@ -6,8 +6,21 @@ function custom_shell_init
   abbr b bin/rails
 
   set -Ux EDITOR 'code --wait'
+  set -Ux DOKKU_HOST 'dokku.gshaw.ca'
+
+  # Maintain IEx history
+  # https://stackoverflow.com/a/45405071/265940
+  set -Ux ERL_AFLAGS '-kernel shell_history enabled'
+
+  # Setup dokku
+  alias dokku 'bash $HOME/.dokku/contrib/dokku_client.sh --rm'
 
   # Setup rbenv
-  set PATH $HOME/.rbenv/shims $PATH
-  rbenv rehash >/dev/null ^&1
+  status --is-interactive; and source (rbenv init -|psub)
+
+  # Setup asdf
+  source /usr/local/opt/asdf/asdf.fish
+
+  # Setup Rust
+  # set PATH $HOME/.cargo/bin $PATH
 end
