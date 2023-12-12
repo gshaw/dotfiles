c89662486d9acb9a570e29ec1cd9081a031b0717
Gerry Shaw <gerry_shaw@yahoo.com>
2023-12-12 12:03:23 -0800

    Recent updates to config

--- fish/functions/custom_shell_init.fish
+++ fish/functions/custom_shell_init.fish
@@ -1,6 +1,7 @@
 function custom_shell_init
   abbr g git
   abbr m mix
+  abbr l eza
   abbr lbs /Users/gerry/.asdf/installs/elixir/1.13.0/.mix/escripts/livebook server
   abbr sl swiftlint .
   abbr sf swiftformat .
@@ -12,7 +13,7 @@ function custom_shell_init
   # abbr devlog tail -f log/development.log
   # abbr guard bundle exec guard
 
-  set -Ux EDITOR 'code --wait'
+  set -Ux EDITOR 'code --wait --new-window'
   set -Ux DOKKU_HOST 'dokku.gshaw.ca'
 
   # Setup Homebrew
@@ -23,8 +24,8 @@ function custom_shell_init
   # https://stackoverflow.com/a/45405071/265940
   set -Ux ERL_AFLAGS '-kernel shell_history enabled'
 
-  # Setup asdf
-  source /opt/homebrew/opt/asdf/libexec/asdf.fish
+  # Setup .NET
+  set -Ux DOTNET_ROOT (rtx where dotnet)
 
   # Setup Rust
   # set PATH $HOME/.cargo/bin $PATH
